f8b05d0ce32a41a2f183db0771edd108286e3684
Faysal Sharif <faysal@faysals-macbook-pro.local>
2018-07-24 15:49:13 -0500

    Initial Commit

---
+++ LNApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LNApp
+//
+//  Created by Faysal Sharif on 7/24/18.
+//  Copyright © 2018 Faysal Sharif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
