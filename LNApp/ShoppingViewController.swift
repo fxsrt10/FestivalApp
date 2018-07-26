c4d06568e1e3196b2fdaa72c4d7d360eac1cdbf6
Faysal Sharif <faysal@dhcp-10-154-122-97.vpn.oracle.com>
2018-07-26 15:25:28 -0500

    Added logos and modified ui of shopping view

---
+++ LNApp/ShoppingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ShoppingViewController.swift
+//  LNApp
+//
+//  Created by Faysal Sharif on 7/26/18.
+//  Copyright © 2018 Faysal Sharif. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingViewController: UIViewController {
+    @IBOutlet weak var invoiceTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
