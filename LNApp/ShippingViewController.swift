21d75e7921bef4ee379db451e35b1dfb41ce167d
carsontmoore <carsontmoore@gmail.com>
2018-07-30 16:26:52 -0500

    Adds additional shopping screens

---
+++ LNApp/ShippingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShippingViewController.swift
+//  LNApp
+//
+//  Created by Carson Moore on 7/30/18.
+//  Copyright © 2018 Faysal Sharif. All rights reserved.
+//
+
+import UIKit
+
+class ShippingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
